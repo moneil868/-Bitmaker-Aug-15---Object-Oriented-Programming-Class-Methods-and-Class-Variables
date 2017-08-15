1d907affca2825ce0c974e728a394a4fb15a63a8
Marlon O'Neil <hello@marlononeil.com>
2017-08-15 14:48:55 -0400

    completed Book Lender tasks 1-3

---
+++ bookLending.rb
@@ -0,0 +1,20 @@
+# Create a class called Book.
+class book
+
+  # Your class should have two class variables: @@on_shelf and @@on_loan. Both should start as empty arrays. @@on_shelf will contain the collection of book objects that are available to be lent out and @@on_loan will contain the collection of books that are currently being borrowed.
+  @@on_shelf = []
+  @@on_loan = []
+
+
+
+
+  # Your class will also need reader and writer methods for due_date.
+  def self.get_due_date
+
+  end
+
+  def self.set_due_date
+
+  end
+
+end
