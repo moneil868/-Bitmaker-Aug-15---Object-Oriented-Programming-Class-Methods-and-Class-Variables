098b32d66b959cb5558e635d78bec2400c8f2f44
Marlon O'Neil <hello@marlononeil.com>
2017-08-17 21:08:16 -0400

    completed Part 2: Book Lending

--- bookLending.rb
+++ bookLending.rb
@@ -4,12 +4,15 @@ class Book
   # Your class should have two class variables: @@on_shelf and @@on_loan. Both should start as empty arrays. @@on_shelf will contain the collection of book objects that are available to be lent out and @@on_loan will contain the collection of books that are currently being borrowed.
   @@on_shelf = []
   @@on_loan = []
+  @@due_date = Time.now
 
   # Your class will also need reader and writer methods for due_date.
-  def self.get_due_date
+  def self.due_date
+    @due_date
   end
 
-  def self.set_due_date
+  def self.due_date=(due_date)
+
   end
 
   # a class method create
@@ -21,11 +24,17 @@ class Book
   end
 
   # a class method current_due_date
-  def self.currnet_due_date
+  def self.current_due_date
+    Time.now + 60
   end
 
   # a class method overdue_books
   def self.overdue_books
+    @@on_loan.each do |book|
+      if @@due_date < Time.now
+        return book
+      end
+    end
   end
 
   # a class method browse
@@ -46,6 +55,9 @@ class Book
     @@on_loan
   end
 
+  def due_date
+    @due_date
+  end
 
 
   # an instance method initialize
@@ -55,28 +67,47 @@ class Book
     @author = author
     @isbn = isbn
 
+    @due_date = @@due_date
   end
 
   # an instance method borrow
   def borrow
     # This instance method is how a book is taken out of the library. Otherwise, use current_due_date to set the due_date of the book and move it from the collection of available books to the collection of books on loan, then return true.
-
     # This method should use lent_out? to check if the book is already on loan,
-
+    if self.lent_out? == false
+      @due_date = Book.current_due_date
+      @@on_shelf.delete(self)
+      @@on_loan << self
+      return true
+    else
+      return false
+    end
 
   end
 
   # an instance method return_to_library
   def return_to_library
+
+    if self.lent_out?
+      puts @@on_loan.delete(self)
+      @@on_shelf << self
+      @@due_date = nil
+      return true
+    else
+      return false
+    end
+
   end
 
   # an instance method lent_out
   def lent_out?
-    puts @@on_shelf
-
-
-
+    @@on_loan.include?(self)
   end
+
+  def get_book_name
+    @title
+  end
+
 
 end
 
@@ -84,11 +115,16 @@ end
 sister_outsider = Book.create("Sister Outsider", "Audre Lorde", "9781515905431")
 aint_i = Book.create("Ain't I a Woman?", "Bell Hooks", "9780896081307")
 if_they_come = Book.create("If They Come in the Morning", "Angela Y. Davis", "0893880221")
-
-# puts Book.browse.inspect
-# puts Book.browse.inspect
-# puts Book.browse.inspect
-# puts Book.available.inspect
-# puts Book.borrowed.inspect
-puts sister_outsider.lent_out?
-# puts sister_outsider.borrow
+puts Book.borrowed.inspect # []
+puts sister_outsider.lent_out? # false
+puts sister_outsider.borrow # true
+puts sister_outsider.lent_out? # true
+puts sister_outsider.borrow # false
+puts sister_outsider.due_date # 2017-02-25 20:52:20 -0500 (this value will be different for you)
+puts Book.available.inspect # [#<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307">, #<Book:0x00562314675fd8 @title="If They Come in the Morning", @author="Angela Y. Davis", @isbn="0893880221">]
+puts Book.borrowed.inspect # [#<Book:0x00562314676208 @title="Sister Outsider", @author="Audre Lorde", @isbn="9781515905431", @due_date=2017-02-25 20:55:17 -0500>]
+puts Book.overdue_books.inspect # []
+puts sister_outsider.return_to_library # true
+puts sister_outsider.lent_out? # false
+puts Book.available.inspect # [#<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307">, #<Book:0x00562314675fd8 @title="If They Come in the Morning", @author="Angela Y. Davis", @isbn="0893880221">, #<Book:0x00562314676208 @title="Sister Outsider", @author="Audre Lorde", @isbn="9781515905431", @due_date=nil>]
+puts Book.borrowed.inspect # []
