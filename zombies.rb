f3d5cdf1abec63c7281220a7d3ae94d1426236c8
Marlon O'Neil <hello@marlononeil.com>
2017-08-18 21:34:20 -0400

    Completed part 3

--- zombies.rb
+++ zombies.rb
@@ -15,10 +15,21 @@ class Zombie
 
   #This class method represents the events of yet another day of the zombie apocalypse. Every day some zombies die off (phew!), some new ones show up, and sometimes the zombie plague level increases. In order to accomplish this, new_day should call some_die_off, spawn, and increase_plague_level.
   def self.new_day
+    some_die_off
+    spawn
+    increase_plague_level
+  end
+
+  def self.default_strength
+    @@default_strength
   end
 
   # This class method should generate a random number between 0 and 10 and remove that number of zombies from @@horde (hint: you may want to check the Array docs for methods that could help you accomplish this).
   def self.some_die_off
+    random_number = rand(0..10)
+    random_number.times do
+      @@horde.delete_at(rand(@@horde.length))
+    end
   end
 
   #This class method should use @@plague_level to generate a random number and then create that number of new zombies, adding each one to @@horde. Use @@max_speed and @@max_strength to generate random values for each new zombie's speed and strength.
@@ -33,39 +44,60 @@ class Zombie
 
   # This class method should generate a random number between 0 and 2 and increase @@plague_level by that amount.
   def self.increase_plague_level
+    random_number = rand(2)
+    @@plague_level += random_number
   end
+
 
   #Instance Methods
 
   # This instance method should take two integer arguments: one for the zombie's speed attribute and one for its strength.
-  def initialize(speed = @default_speed, strength = @default_speed)
+  def initialize(speed = @@default_speed, strength = @@default_strength)
 
     # If the speed argument is greater than @@max_speed then @@default_speed should be used as the new zombie's speed instead.
     @speed = speed
-    if speed > @@max_speed
+    if @speed > @@max_speed
       @speed = @default_speed
       return @speed
     end
 
     # Similarly, you should check to make sure the strength argument is less than or equal to @@max_strength, otherwise @@default_strength should be used as the new zombie's strength attribute.
     @strength = strength
-    if strength <= @@max_strength
-      @strength = @default_strength
-      return @strength
+    if @strength <= @@max_strength
+      @strength = @@default_strength
+        return @strength
     end
-    
+
   end
 
   def encounter
+    if outrun_zombie?
+      puts "Congratulations, you outran the zombie!"
+    elsif survive_attack?
+      puts "Your strength is high, you survived the attack!"
+    else
+      puts "The zombie got you, you are now a zombie."
+      new_zombie = Zombie.new
+      @@horde << new_zombie
+    end
+
   end
 
   def outrun_zombie?
+    @speed > rand(@@max_speed)
   end
 
   def survive_attack?
+    @strength > rand(@@max_strength)
   end
 
 end
 
-
-zombie = Zombie.spawn
+puts "============================+ TESTS +=========================="
+puts Zombie.all.inspect
+Zombie.new_day
+puts Zombie.all.inspect
+zombie1 = Zombie.all[0]
+zombie2 = Zombie.all[1]
+zombie3 = Zombie.all[2]
+puts zombie1.encounter
